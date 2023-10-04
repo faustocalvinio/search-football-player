e483296d7c4338b807160a485f0cd9f45ab7ab11
Fausto Calviño <faustocalvino@outlook.com>
2023-10-04 12:55:59 +0200

    feat: gallery modal, card styles, no results fallback

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import messiMockImage from './mocks/messi-thumb.jpg';
 import Modal from 'react-modal';
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-import { ThemeSwitcher } from "./components/themeSwitcher";
+import { ThemeSwitcher } from "./components/ThemeSwitcher";
 const images2=[
   {original:'./mocks/fanart-1.png',thumbnail:'./mocks/fanart-1.png'},
   {original:'./mocks/fanart-2.png',thumbnail:'./mocks/fanart-2.png'},
@@ -13,7 +13,7 @@ const images2=[
   {original:'./mocks/fanart-5.png',thumbnail:'./mocks/fanart-5.png'},
   
 ]
-const images = [
+const images23 = [
   {
     original: "https://picsum.photos/id/1018/1000/600/",
     thumbnail: "https://picsum.photos/id/1018/250/150/",
@@ -31,6 +31,7 @@ Modal.setAppElement('#root');
 
 function App() {
   const [textModalStatus, setTextModalStatus] = useState(false)
+  // const [images, setImages] = useState(images23)
   const [galleryModalStatus, setGalleryModalStatus] = useState(false)
   const inputValue = useRef();
   const toggleTextModal = ()=>{
@@ -43,7 +44,7 @@ function App() {
   }
   const [playerData, setPlayerData] = useState(testPlayerData)
 
-  
+  console.log(playerData.player[0].strFanart1);
 
   const onSearchSubmit = (e)=>{
     e.preventDefault()
@@ -57,26 +58,34 @@ function App() {
     } catch (error) {
       console.log(error);
     }
+    
+    inputValue.current.value='';
   }
 
-  const textShort=  playerData.player[0].strDescriptionEN.slice(0,400)
+  // const textShort = playerData.player !== null || playerData.player[0].strSport === 'Soccer' ?  playerData.player[0].strDescriptionEN.slice(0,400) : ''
   return (
     <>
-    <ThemeSwitcher />
+    {/* <ThemeSwitcher /> */}
     <div className="flex flex-col max-w-[1300px] mx-auto">
-      <h1 className="text-red-700 text-5xl text-center">Player Data</h1>
-      
+      <div className="flex flex-row justify-between w-[600px] mx-auto">
+        <h1 className="text-red-700 text-5xl text-center">Player Data</h1>
+        <ThemeSwitcher />
+      </div>
       <form onSubmit={onSearchSubmit} className="flex gap-2 w-[600px] mx-auto py-7">
       <input ref={inputValue} type="text" placeholder="Search player" className="p-2 rounded-lg border-2 w-[80%]" />
       <button type="submit" className="bg-red-700 text-white p-2 rounded-lg w-[20%]">Search</button>
       </form>
-      <main className="relative bg-gray-500 flex flex-col p-4 rounded-lg mt-9 mb-8 shadow-lg text-white text-center w-full max-w-[600px] mx-auto border-2 border-red-700/50">
+      {
+        playerData.player===null || playerData.player[0].strSport !== 'Soccer' ? <h1 className="text-5xl font-extrabold dark:text-white">No results found</h1> : (
+          <main className="relative bg-gray-500 flex flex-col p-4 rounded-lg mt-9 mb-8 shadow-lg text-white text-center w-full max-w-[600px] mx-auto border-2 border-red-700/50">
        
-        <h2>{ JSON.stringify(playerData.player[0].strPlayer)  }</h2>
-        <h4>{playerData.player[0].strPosition}</h4>
-        <h4 className="absolute left-[-40px] top-[-60px] rotate-[-45deg] text-[80px]">{playerData.player[0].strNumber}</h4>
-        <h4 className="absolute right-[-40px] top-[-60px] rotate-[45deg] text-[80px]">{playerData.player[0].strNumber}</h4>
-        <p className="text-custom">status: { playerData.player[0].strStatus==="Active" ? 'Active ✅' : 'Retired ❌' }</p>
+        <h1 className="text-5xl font-extrabold dark:text-white">{ JSON.stringify(playerData.player[0].strPlayer)  }</h1>
+
+        
+        <h4 className="text-2xl underline underline-offset-3 decoration-2 decoration-blue-600 dark:decoration-blue-100">{playerData.player[0].strPosition}</h4>
+        {/* <h4 className="absolute left-[-40px] top-[-60px] rotate-[-45deg] text-[80px]">{playerData.player[0].strNumber}</h4> */}
+        {/* <h4 className="absolute right-[-40px] top-[-60px] rotate-[45deg] text-[80px]">{playerData.player[0].strNumber}</h4> */}
+        <p className="text-lg">{ playerData.player[0].strStatus==="Active" ? 'Active ✅' : 'Retired ❌' }</p>
         <p><strong>Nationality:</strong> {playerData.player[0].strNationality}</p>
         <p><strong>Place and date of birth:</strong> {playerData.player[0].strBirthLocation}📍 , {playerData.player[0].dateBorn} 📅</p>
         {
@@ -84,25 +93,45 @@ function App() {
           ? <p>Currently Playing for {playerData.player[0].strTeam} ⚽</p>
           : "Retired soccer"
         }
-        <img src={playerData.player[0].strThumb} className="w-[350px] mx-auto" alt="" />
+        <div className="flex flex-row items-center justify-between">
+          <h3 className="text-5xl text-orange-300 mx-0">{playerData.player[0].strNumber}</h3>
+          <img src={playerData.player[0].strThumb} className="w-[350px] mx-0" alt="" />
+          <h3 className="text-5xl text-orange-300 mx-0">{playerData.player[0].strNumber}</h3>
+        </div>
         <button className="bg-red-600 border-2 w-fit mx-auto" onClick={toggleGalleryModal}>View more images</button>
         <Modal isOpen={galleryModalStatus}>
           <button  onClick={toggleGalleryModal}>X</button>
-          <ImageGallery items={images} />
+          <ImageGallery items={[{
+              original: playerData.player[0].strFanart1,
+              thumbnail: playerData.player[0].strFanart1,
+              },{
+              original: playerData.player[0].strFanart2,
+              thumbnail: playerData.player[0].strFanart2,
+              },{
+              original: playerData.player[0].strFanart3,
+              thumbnail: playerData.player[0].strFanart3,
+              },{
+              original: playerData.player[0].strFanart4,
+              thumbnail: playerData.player[0].strFanart4,
+              },]}
+          />
 
         </Modal>
-        {/* <p>{playerData.player[0].strDescriptionEN}</p> */}
-        <p>{textShort}</p>
+        <p>{playerData.player[0].strDescriptionEN.slice(0,400)}</p>
+        {/* <p>{textShort}</p> */}
         <button className="bg-red-600 border-2 w-fit mx-auto" onClick={toggleTextModal}>read more</button>
         <Modal
           isOpen={textModalStatus}
-          className="max-w-[1300px] mx-auto bg-red-200 h-screen"
+          className="max-w-[600px] px-10 mx-auto bg-red-200 h-screen"
           > 
           <button onClick={toggleTextModal}>close</button>
           <p>{playerData.player[0].strDescriptionEN}</p>
         </Modal>
       </main>
 
+        )
+      }
+      
     </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ export default {
   ],
   theme: {
     extend: {
-      darkMode:'class'
+      // darkMode:'class',
+      darkMode:'media'
     },
   },
   plugins: [
