3ad326b432eb0aeded41e51b6f0aa15595ad03ce
Fausto Calviño <faustocalvino@outlook.com>
2023-10-12 23:51:29 +0200

    feat: working on responsive and fixing some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ Modal.setAppElement('#root');
 
 function App() {
   const [textModalStatus, setTextModalStatus] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
   // const [images, setImages] = useState(images23)
   const [galleryModalStatus, setGalleryModalStatus] = useState(false)
   const inputValue = useRef();
@@ -85,12 +85,12 @@ function App() {
     <>
     {/* <ThemeSwitcher /> */}
     <div className="flex flex-col max-w-[1300px] mx-auto py-10">
-      <div className="flex flex-row justify-between w-[600px] mx-auto">
-        <h1 className="text-red-700 text-3xl text-center dark:text-white">Football Player/Manager Data</h1>
+      <div className="flex flex-row justify-between w-[600px] mx-auto max-md:w-[90%]">
+        <h1 className="text-red-700 text-3xl text-center dark:text-white ">Football Player/Manager Data</h1>
         <ThemeSwitcher />
       </div>
-      <form onSubmit={onSearchSubmit} className="flex gap-2 w-[600px] mx-auto py-7">
-      <input ref={inputValue} type="text" placeholder="Search player" className="py-3 px-4 rounded-lg border-2 w-[80%] dark:bg-gray-200 dark:text-black dark:placeholder:text-black" />
+      <form onSubmit={onSearchSubmit} className="flex gap-2 w-[600px] mx-auto py-2 max-md:w-[90%]">
+      <input ref={inputValue} type="text" placeholder="Search player" className="py-2 px-4 rounded-lg border-2 w-[80%] dark:bg-gray-200 dark:text-black dark:placeholder:text-black" />
       <button type="submit" className="bg-blue-700 text-white p-2 rounded-lg w-[20%] dark:bg-blue-900 dark:hover:bg-blue-800"  >Search</button>
       </form>
       {
@@ -100,8 +100,8 @@ function App() {
        
 
           
-          <main className="bg-gray-400 dark:bg-gray-800 relative flex flex-col p-4 rounded-lg mt-9 mb-8 shadow-lg text-white text-center w-full max-w-[600px] mx-auto border-2 border-red-700/50 gap-3">
-       
+          <main className="bg-gray-400 dark:bg-gray-800 relative flex flex-col p-4 rounded-lg mt-2 mb-8 shadow-lg text-white text-center w-full max-w-[600px] mx-auto border-2 border-red-700/50 gap-3 max-md:w-[90%]">
+       {isLoading ? <h1 className="text-white">Loading</h1> : <h2 className="text-white">Loaded</h2>}
         <h1 className="text-5xl font-extrabold dark:text-white">{ JSON.stringify(playerData.player[0].strPlayer)  }</h1>
 
         
@@ -116,11 +116,11 @@ function App() {
           : "Retired soccer"
         }
         <div className="flex flex-row items-center justify-between">
-          <h3 className="text-5xl text-white mx-0">{playerData.player[0].strNumber}</h3>
-          <img src={playerData.player[0].strThumb} className="w-[350px] mx-0" alt="" />
-          <h3 className="text-5xl text-white mx-0">{playerData.player[0].strNumber}</h3>
+          <h3 className="text-5xl text-white mx-0 max-[570px]:hidden">{playerData.player[0].strNumber}</h3>
+          <img src={playerData.player[0].strThumb} className="w-[350px] mx-0 max-[570px]:w-full" alt="" />
+          <h3 className="text-5xl text-white mx-0 max-[570px]:hidden">{playerData.player[0].strNumber}</h3>
         </div>
-        <button className="bg-red-600 border-0 py-2 px-4 w-fit mx-auto dark:bg-blue-700 dark:hover:bg-blue-600" disabled={playerData.player[0].strFanart1 === null ? true : false} onClick={toggleGalleryModal}>View fanarts images</button>
+        <button className={`${playerData.player[0].strFanart1 === null ? 'hidden' : 'bg-red-600 border-0 py-2 px-4 w-fit mx-auto dark:bg-blue-700 dark:hover:bg-blue-600'}`} disabled={playerData.player[0].strFanart1 === null ? true : false} onClick={toggleGalleryModal}>View fanarts images</button>
         <Modal  isOpen={galleryModalStatus}>
           <button  onClick={toggleGalleryModal}>X</button>
           <ImageGallery items={[{
@@ -139,7 +139,7 @@ function App() {
           />
 
         </Modal>
-        <p>{playerData.player[0].strDescriptionEN.slice(0,400)} ...</p>        
+        <p>{ playerData.player[0].strDescriptionEN===null ? 'no descripption'  : playerData.player[0].strDescriptionEN.slice(0,400)} ...</p>        
         <button className="bg-red-600 border-0 py-2 px-4 w-fit mx-auto dark:bg-blue-700 dark:hover:bg-blue-600" onClick={toggleTextModal}>Read More</button>
         <Modal
           isOpen={textModalStatus}
@@ -152,7 +152,7 @@ function App() {
       
         
       }
-      {isLoading ? <h1 className="text-white">Loading</h1> : <h2 className="text-white">Loaded</h2>}
+      
       <footer className=""> <p className="text-center dark:text-white">Made with React and  <a href="https://www.thesportsdb.com/" target="_blank" rel="noreferrer" className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">TheSportsDB</a>. Source code of this project <a href="https://github.com/faustocalvinio/search-football-player" target="_blank" rel="noreferrer"  className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">here</a>.</p>  </footer>
     </div>
     </>
